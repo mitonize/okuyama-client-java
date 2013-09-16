dd52e6f582f0ed276d7f1a1fa5ffa6e9c52c9f55
Mitsutaka Watanabe <mitonize@gmail.com>
2013-09-16 22:46:58 +0900

    Add some sample sources

---
+++ src/main/java/jp/co/unisys/web/datastore/okuyama/Example.java
@@ -0,0 +1,15 @@
+package jp.co.unisys.web.datastore.okuyama;
+
+public class Example {
+	private String name;
+
+	public Example() {
+	}
+	public Example(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
