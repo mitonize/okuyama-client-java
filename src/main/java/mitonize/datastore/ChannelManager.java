49a5acb2c4861f1583091da8c4fe13dba52db231
Mitsutaka Watanabe <mitonize@gmail.com>
2013-11-07 06:59:26 +0900

    ChannelManagerでアクティブなチャネル数を計測する。

--- src/main/java/mitonize/datastore/ChannelManager.java
+++ src/main/java/mitonize/datastore/ChannelManager.java
@@ -3,12 +3,18 @@ package mitonize.datastore;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.UnresolvedAddressException;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChannelManager {
+	Logger logger = LoggerFactory.getLogger(ChannelManager.class);
 	/**
 	 * 接続先のTCPエンドポイントを保持
 	 */
@@ -20,9 +26,11 @@ public class ChannelManager {
 	int currentEndpointIndex = 0;
 
 	protected ArrayBlockingQueue<SocketChannel> queue;
+	protected AtomicInteger activeSocket;
 
 	public ChannelManager(String[] masternodes, int maxPoolSize) throws UnknownHostException {
 		queue = new ArrayBlockingQueue<SocketChannel>(maxPoolSize);
+		activeSocket = new AtomicInteger(0);
 		endpoints = new Endpoint[masternodes.length];
 		for (int i = 0; i < masternodes.length; ++i) {
 			endpoints[i] = new Endpoint();
@@ -32,6 +40,7 @@ public class ChannelManager {
 	}
 
 	public SocketChannel aquire() throws IOException {
+//		logger.info("Socket aquire");
 		SocketChannel channel = queue.poll();
         return channel == null || !isAvailable(channel) ? openSocket() : channel;
 	}
@@ -39,6 +48,9 @@ public class ChannelManager {
 	public void recycle(SocketChannel channel) {
 		if (channel != null && !queue.offer(channel)) {
 			closeSocket(channel);
+//			logger.info("Socket close");
+		} else {
+//			logger.info("Socket recycle");
 		}
 	}
 
@@ -56,6 +68,8 @@ public class ChannelManager {
 		try {
 			InetSocketAddress address = new InetSocketAddress (endpoint.address, endpoint.port);
 			SocketChannel channel = SocketChannel.open(address);
+			int c = activeSocket.incrementAndGet();
+			logger.info("Socket opened -  count:" + c);
 			return channel;
 		} catch (UnresolvedAddressException e) {
 			return null;
@@ -65,6 +79,8 @@ public class ChannelManager {
 	void closeSocket(SocketChannel socketChannel) {
 		try {
 			socketChannel.close();
+			int c = activeSocket.decrementAndGet();
+			logger.info("Socket closed - count:" + c);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
