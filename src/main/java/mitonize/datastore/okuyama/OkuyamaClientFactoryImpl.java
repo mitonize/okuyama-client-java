ad80682afb7f46f3a0e97bc143577cf919e5b86f
Mitsutaka Watanabe <mitonize@gmail.com>
2013-11-11 02:02:22 +0900

    Discard SocketChannel version

--- src/main/java/mitonize/datastore/okuyama/OkuyamaClientFactoryImpl.java
+++ src/main/java/mitonize/datastore/okuyama/OkuyamaClientFactoryImpl.java
@@ -2,31 +2,22 @@ package mitonize.datastore.okuyama;
 
 import java.net.UnknownHostException;
 
-import mitonize.datastore.ChannelManager;
 import mitonize.datastore.SocketManager;
 
 
 public class OkuyamaClientFactoryImpl extends OkuyamaClientFactory {
 	SocketManager socketManager;
-	ChannelManager channelManager;
-	boolean useSocketChannel = true;
 
 	@Override
 	public OkuyamaClient createClient() {
 		OkuyamaClient okuyamaClient;
-		if (useSocketChannel) {
-			okuyamaClient = new OkuyamaClientImpl(channelManager);
-		} else {
-			okuyamaClient = new OkuyamaClientImpl2(socketManager);
-		}
+		okuyamaClient = new OkuyamaClientImpl2(socketManager);
 		return okuyamaClient;
 	}
 	
-	public OkuyamaClientFactoryImpl(String[] masternodes, boolean useSocketChannel) throws UnknownHostException {
+	public OkuyamaClientFactoryImpl(String[] masternodes) throws UnknownHostException {
 		super.setMasterNodes(masternodes);
-		channelManager = new ChannelManager(masternodes, 5);
 		socketManager = new SocketManager(masternodes, 5);
-		this.useSocketChannel = useSocketChannel;
 	}
 
 }
